4369396a8858bbc4c17a63084ca9f5963d0586bf
94leonardo <lassoleonardo56@gmail.com>
2024-05-16 22:52:26 -0500

    update name imagens

---
+++ src/componentes/1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img11 from '../images/1.jpg'
+
+const img1 = () => {
+    return (
+        <div>
+            <img src={img11} alt="juegos" />
+        </div>
+    )
+}
+
+export default 1
